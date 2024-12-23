4ad2d779edb42ab5e2edd5c1916fd807d0045132
Xavier RODRIGUEZ <xavier.rodriguezalvarez@gmail.com>
2024-12-23 06:04:32 +0100

    Trigger (#4)

--- src/daq/device.rs
+++ src/daq/device.rs
@@ -2,7 +2,7 @@ use async_trait::async_trait;
 use panduza_platform_core::{DriverOperations, Error, Instance};
 use std::time::Duration;
 use tokio::time::sleep;
-mod eval_echo;
+mod random_si_reader;
 
 #[derive(Default)]
 ///
@@ -16,8 +16,17 @@ impl DriverOperations for Device {
     ///
     ///
     async fn mount(&mut self, instance: Instance) -> Result<(), Error> {
-        let driver = eval_echo::EvalEcho::default().into_arc_mutex();
-        panduza_platform_core::std::class::repl::mount("repl", instance.clone(), driver).await?;
+        let interface = random_si_reader::RandomSiReader::default().into_arc_mutex();
+        panduza_platform_core::std::class::acq_si::mount(
+            "randommeter",
+            "Pika",
+            0.0,
+            0xffff as f64,
+            4,
+            instance.clone(),
+            interface.clone(),
+        )
+        .await?;
         Ok(())
     }
     ///
--- src/lib.rs
+++ src/lib.rs
@@ -5,14 +5,16 @@ panduza_platform_core::plugin_interface!("vi");
 
 //
 // Import modules
-mod repl;
 mod attribute_tester;
+mod daq;
+mod repl;
 
 //
 // Export the producers of the plugin
 pub fn plugin_producers() -> Vec<Box<dyn Producer>> {
     let mut producers: Vec<Box<dyn Producer>> = vec![];
     producers.push(repl::Package::default().boxed());
+    producers.push(daq::Package::default().boxed());
     producers.push(attribute_tester::Package::default().boxed());
     return producers;
 }
