d117c678d40a656f48223dd82873c0df1f2808f1
Xavier RODRIGUEZ <xavier.rodriguezalvarez@gmail.com>
2025-01-05 10:03:54 +0100

    Log printer (#14)
    * simple plugin to log every sec
    
    * prepare 1.0.0

---
+++ src/platform_logger/device.rs
@@ -0,0 +1,34 @@
+use async_trait::async_trait;
+use panduza_platform_core::{log_info, spawn_loop, Container, DriverOperations, Error, Instance};
+use std::time::Duration;
+use tokio::time::sleep;
+
+#[derive(Default)]
+///
+///
+pub struct Device {}
+
+#[async_trait]
+impl DriverOperations for Device {
+    ///
+    ///
+    ///
+    async fn mount(&mut self, instance: Instance) -> Result<(), Error> {
+        let mut instance_2 = instance.clone();
+        spawn_loop!("test", instance_2, {
+            let mut counter: u64 = 0;
+            loop {
+                log_info!(instance.logger(), "Hello {}", counter);
+                tokio::time::sleep(Duration::from_secs(1)).await;
+                counter += 1;
+            }
+        });
+        Ok(())
+    }
+    ///
+    /// Easiest way to implement the reboot event
+    ///
+    async fn wait_reboot_event(&mut self, mut _device: Instance) {
+        sleep(Duration::from_secs(5)).await;
+    }
+}
