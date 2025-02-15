d27b1d23fa5d5303f0de4945656925cc4a8593be
XdoctorwhoZ <xavier.rodriguezalvarez@gmail.com>
2025-02-15 20:47:08 +0100

    up

--- src/lib.rs
+++ src/lib.rs
@@ -1,31 +1,31 @@
-use panduza_platform_core::{Producer, Scanner};
+use panduza_platform_core::Producer;
 
 #[cfg(feature = "plugin")]
-panduza_platform_core::plugin_interface!("vi");
+panduza_platform_core::plugin_interface!("vi", "0.0.0");
 
 //
 // Import modules
-mod boolean_vector;
-mod daq;
-mod platform_logger;
-mod repl;
-mod tester;
+// mod boolean_vector;
+// mod daq;
+// mod platform_logger;
+// mod repl;
+// mod tester;
 
 //
 // Export the producers of the plugin
-pub fn plugin_producers() -> Vec<Box<dyn Producer>> {
-    let mut producers: Vec<Box<dyn Producer>> = vec![];
-    producers.push(repl::Package::default().boxed());
-    producers.push(daq::Package::default().boxed());
-    producers.push(tester::Package::default().boxed());
-    producers.push(platform_logger::Package::default().boxed());
-    producers.push(boolean_vector::Package::default().boxed());
+pub fn plugin_producers<O>() -> Vec<Box<dyn Producer<O>>> {
+    let mut producers: Vec<Box<dyn Producer<O>>> = vec![];
+    // producers.push(repl::Package::default().boxed());
+    // producers.push(daq::Package::default().boxed());
+    // producers.push(tester::Package::default().boxed());
+    // producers.push(platform_logger::Package::default().boxed());
+    // producers.push(boolean_vector::Package::default().boxed());
     return producers;
 }
 
 //
 //
-pub fn plugin_scanners() -> Vec<Box<dyn Scanner>> {
-    let scanners: Vec<Box<dyn Scanner>> = vec![];
-    return scanners;
-}
+// pub fn plugin_scanners() -> Vec<Box<dyn Scanner>> {
+//     let scanners: Vec<Box<dyn Scanner>> = vec![];
+//     return scanners;
+// }
