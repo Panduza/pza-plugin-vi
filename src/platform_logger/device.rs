f295cb55602d10fed73a0a766f9256db7980f060
XdoctorwhoZ <xavier.rodriguezalvarez@gmail.com>
2025-02-16 07:43:11 +0100

    up

--- src/platform_logger/device.rs
+++ src/platform_logger/device.rs
@@ -1,5 +1,5 @@
 use async_trait::async_trait;
-use panduza_platform_core::{log_info, spawn_loop, Actions, Container, Error, Instance};
+use panduza_platform_core::{log_info, Actions, Container, Error, Instance};
 use std::time::Duration;
 use tokio::time::sleep;
 
@@ -14,21 +14,20 @@ impl Actions for Device {
     ///
     ///
     async fn mount(&mut self, instance: Instance) -> Result<(), Error> {
-        let mut instance_2 = instance.clone();
-        // spawn_loop!("test", instance_2, {
-        //     let mut counter: u64 = 0;
-        //     loop {
-        //         log_info!(instance.logger(), "Hello {}", counter);
-        //         tokio::time::sleep(Duration::from_secs(1)).await;
-        //         counter += 1;
-        //     }
-        // });
+        tokio::spawn(async move {
+            let mut counter: u64 = 0;
+            loop {
+                log_info!(instance.logger(), "Hello {}", counter);
+                tokio::time::sleep(Duration::from_secs(1)).await;
+                counter += 1;
+            }
+        });
         Ok(())
     }
     ///
     /// Easiest way to implement the reboot event
     ///
-    async fn wait_reboot_event(&mut self, mut instance: Instance) {
+    async fn wait_reboot_event(&mut self, _: Instance) {
         sleep(Duration::from_secs(5)).await;
     }
 }
