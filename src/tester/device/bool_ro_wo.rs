02c8738acf7d7f3a1145e0ec9300656db282cc1a
Xavier RODRIGUEZ <xavier.rodriguezalvarez@gmail.com>
2025-02-18 09:25:47 +0100

    up

--- src/tester/device/bool_ro_wo.rs
+++ src/tester/device/bool_ro_wo.rs
@@ -28,112 +28,89 @@ Contrairement à une opinion répandue, le Lorem Ipsum n'est pas simplement du t
 aléatoire. Il trouve ses racines dans une oeuvre de la littérature latine classique datant de 45 av. J.-C., le rendant vieux de 2000 ans. Un professeur du Hampden-Sydney College, en Virginie, s'est intéressé à un des mots latins les plus obscurs, consectetur, extrait d'un passage du Lorem Ipsum, 
 et en étudiant tous les usages de ce mot dans la littérature classique.
         "#)
-        .finish_as_boolean()
+        .start_as_boolean()
         .await?;
     att_boolean_ro.set(false).await?;
 
     //
     //
-    let att_boolean_wo = class
+    let mut att_boolean_wo = class
         .create_attribute("boolean_wo")
         .with_wo()
         .with_info(
             r#"write command
-# Heading level 1
+    # Heading level 1
 
-I just love **bold text**.
-I just love __bold text__.
-Love**is**bold
+    I just love **bold text**.
+    I just love __bold text__.
+    Love**is**bold
 
-## Heading level 2 
+    ## Heading level 2
 
-Italicized text is the *cat's meow*.
-Italicized text is the _cat's meow_.
+    Italicized text is the *cat's meow*.
+    Italicized text is the _cat's meow_.
 
-### Heading level 3
+    ### Heading level 3
 
-> Dorothy followed her through many of the beautiful rooms in her castle.
-> Dorothy followed her through many of the beautiful rooms in her castle.
-> - Revenue was off the chart.
-> - Profits were higher than ever.
+    > Dorothy followed her through many of the beautiful rooms in her castle.
+    > Dorothy followed her through many of the beautiful rooms in her castle.
+    > - Revenue was off the chart.
+    > - Profits were higher than ever.
 
-
-
-        "#,
+            "#,
         )
-        .finish_as_boolean()
+        .start_as_boolean()
         .await?;
 
-    // //
-    // //
-    // let att_boolean_wo_2 = att_boolean_wo.clone();
-    // spawn_on_command!(
-    //     "on_command",
-    //     instance,
-    //     att_boolean_wo_2,
-    //     on_command(att_boolean_ro.clone(), att_boolean_wo_2.clone())
-    // );
+    //
+    //
+    tokio::spawn(async move {
+        loop {
+            att_boolean_wo.wait_for_commands().await;
+            while let Some(command) = att_boolean_wo.pop().await {
+                log_info!(att_boolean_wo.logger(), "command recieved - {:?}", command);
+                att_boolean_ro.set(command).await.unwrap();
+            }
+        }
+    });
 
     //
     //
-    let att_boolean_rw = class
+    let mut att_boolean_rw = class
         .create_attribute("boolean_rw")
         .with_rw()
         .with_info(
             r#"read write command
-        
-#### Heading level 4
 
-1. First item
-2. Second item
-3. Third item
-4. Fourth item 
+    #### Heading level 4
 
-I really like using Markdown.
-I think I'll use it to format all of my documents from now on. 
+    1. First item
+    2. Second item
+    3. Third item
+    4. Fourth item
 
-My favorite search engine is [Duck Duck Go](https://duckduckgo.com).
+    I really like using Markdown.
+    I think I'll use it to format all of my documents from now on.
 
-        "#,
+    My favorite search engine is [Duck Duck Go](https://duckduckgo.com).
+
+            "#,
         )
-        .finish_as_boolean()
+        .start_as_boolean()
         .await?;
     att_boolean_rw.set(false).await?;
 
-    // //
-    // //
-    // let att_boolean_rw_2 = att_boolean_rw.clone();
-    // spawn_on_command!(
-    //     "on_command => boolean_rw",
-    //     instance,
-    //     att_boolean_rw_2,
-    //     on_command_rw(att_boolean_rw_2.clone())
-    // );
+    //
+    //
+    tokio::spawn(async move {
+        loop {
+            att_boolean_rw.wait_for_commands().await;
+            while let Some(command) = att_boolean_rw.pop().await {
+                log_info!(att_boolean_rw.logger(), "command recieved - {:?}", command);
+                att_boolean_rw.set(command).await.unwrap();
+            }
+        }
+    });
 
     Ok(())
 }
-
-// ///
-// ///
-// ///
-// async fn on_command(
-//     att_boolean_ro: BooleanAttServer,
-//     mut att_boolean_wo: BooleanAttServer,
-// ) -> Result<(), Error> {
-//     while let Some(command) = att_boolean_wo.pop_cmd().await {
-//         log_info!(att_boolean_wo.logger(), "command recieved - {:?}", command);
-//         att_boolean_ro.set(command).await?;
-//     }
-//     Ok(())
-// }
-
-// ///
-// ///
-// ///
-// async fn on_command_rw(mut att_boolean_rw: BooleanAttServer) -> Result<(), Error> {
-//     while let Some(command) = att_boolean_rw.pop_cmd().await {
-//         log_info!(att_boolean_rw.logger(), "command recieved - {:?}", command);
-//         att_boolean_rw.set(command).await?;
-//     }
-//     Ok(())
-// }
