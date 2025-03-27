b0e8fa90589ad57004c6688087dbd11fcddda131
Zisraw <bastien.huard@orange.fr>
2025-03-27 15:14:34 +0100

    add bytes attribute
    send 16kB
    
    generate multiple Bytes classes
    
    mount number of classes as argument
    
    new number + reception time of the platform
    
    commit before rebase

--- src/tester/device/bytes.rs
+++ src/tester/device/bytes.rs
@@ -1,11 +1,12 @@
 use bytes::Bytes;
-use panduza_platform_core::log_debug_mount_end;
-use panduza_platform_core::log_debug_mount_start;
-use panduza_platform_core::log_info;
-use panduza_platform_core::{Container, Error, Instance};
+use panduza_platform_core::{
+    log_debug_mount_end, log_debug_mount_start, log_info, Container, Error, Instance,
+};
+use std::sync::{Arc, Mutex};
+use std::time::Instant;
 
-///
-///
+//
+//
 pub async fn mount(mut instance: Instance) -> Result<(), Error> {
     //
     // Create interface
@@ -26,6 +27,28 @@ pub async fn mount(mut instance: Instance) -> Result<(), Error> {
     att_bytes_ro.set(Bytes::from("initial")).await?;
 
     //
+    // to stock the reception time of "ro"
+    let mut timer_reception = class
+        .create_attribute("counter_ro")
+        .with_ro()
+        .with_info(r#"read command"#)
+        .start_as_number()
+        .await?;
+
+    timer_reception.set(0).await?;
+
+    //
+    // to reset the timer
+    let mut timer_reset = class
+        .create_attribute("counter_reset")
+        .with_wo()
+        .with_info(r#"write command"#)
+        .start_as_boolean()
+        .await?;
+
+    timer_reset.set(true).await?;
+
+    //
     //
     let mut att_bytes_wo = class
         .create_attribute("wo")
@@ -34,14 +57,56 @@ pub async fn mount(mut instance: Instance) -> Result<(), Error> {
         .start_as_bytes()
         .await?;
 
-    //
-    //
+    // tokio::spawn(async move {
+    //     loop {
+    //         att_bytes_wo.wait_for_commands().await;
+    //         while let Some(command) = att_bytes_wo.pop().await {
+    //             log_info!(att_bytes_wo.logger(), "command recieved - {:?}", command);
+    //             att_bytes_ro.set(command).await.unwrap();
+    //         }
+    //     }
+    // });
+
+    // timer shared between reception and reset
+    let shared_timer = Arc::new(Mutex::new(None::<Instant>));
+
+    let shared_timer_reset = shared_timer.clone();
+    let timer_reception_reset = timer_reception.clone();
+
+    // wait reset command and set timer to now and reception to 0
     tokio::spawn(async move {
         loop {
-            att_bytes_wo.wait_for_commands().await;
-            while let Some(command) = att_bytes_wo.pop().await {
-                log_info!(att_bytes_wo.logger(), "command recieved - {:?}", command);
-                att_bytes_ro.set(command).await.unwrap();
+            timer_reset.wait_for_commands().await;
+            while let Some(command) = timer_reset.pop().await {
+                timer_reception_reset.set(0).await.unwrap();
+                {
+                    let mut timer = shared_timer_reset.lock().unwrap();
+                    *timer = Some(Instant::now());
+                }
+            }
+        }
+    });
+
+    //
+    //
+    tokio::spawn({
+        let att_bytes_ro = att_bytes_ro.clone();
+        let shared_timer = shared_timer.clone();
+        let mut counter = 0;
+        async move {
+            loop {
+                att_bytes_wo.wait_for_commands().await;
+                while let Some(command) = att_bytes_wo.pop().await {
+                    att_bytes_ro.set(command).await.unwrap();
+                    if counter % 100 == 0 {
+                        let elapsed = {
+                            let timer = shared_timer.lock().unwrap();
+                            timer.unwrap().elapsed().as_millis() as u32
+                        };
+                        timer_reception.set(elapsed).await.unwrap();
+                    }
+                    counter += 1;
+                }
             }
         }
     });
@@ -54,16 +119,22 @@ pub async fn mount(mut instance: Instance) -> Result<(), Error> {
         .with_info(r#"read write command"#)
         .start_as_bytes()
         .await?;
+
+    //
+    //
     att_bytes_rw.set(Bytes::from("initial")).await?;
 
     //
     //
-    tokio::spawn(async move {
-        loop {
-            att_bytes_rw.wait_for_commands().await;
-            while let Some(command) = att_bytes_rw.pop().await {
-                log_info!(att_bytes_rw.logger(), "command recieved - {:?}", command);
-                att_bytes_rw.set(command).await.unwrap();
+    tokio::spawn({
+        let mut att_bytes_rw = att_bytes_rw.clone();
+        async move {
+            loop {
+                att_bytes_rw.wait_for_commands().await;
+                while let Some(command) = att_bytes_rw.pop().await {
+                    log_info!(att_bytes_rw.logger(), "command received - {:?}", command);
+                    att_bytes_rw.set(command).await.unwrap();
+                }
             }
         }
     });
---
+++ src/tester/device/number.rs
@@ -0,0 +1,64 @@
+use panduza_platform_core::{log_info, Container, Error, Instance};
+
+//
+//
+pub async fn mount(mut instance: Instance) -> Result<(), Error> {
+    //
+    // Create interface
+    let mut class = instance.create_class("counter").finish().await;
+    //
+    //
+    let att_number_ro = class
+        .create_attribute("ro")
+        .with_ro()
+        .with_info(r#"read command"#)
+        .start_as_number()
+        .await?;
+    //
+    //
+    att_number_ro.set(0).await?;
+
+    //
+    //
+    let mut att_number_wo = class
+        .create_attribute("wo")
+        .with_wo()
+        .with_info(r#"write command"#)
+        .start_as_number()
+        .await?;
+
+    //
+    //
+    tokio::spawn(async move {
+        loop {
+            att_number_wo.wait_for_commands().await;
+            while let Some(command) = att_number_wo.pop().await {
+                log_info!(att_number_wo.logger(), "command recieved - {:?}", command);
+                att_number_ro.set(command).await.unwrap();
+            }
+        }
+    });
+
+    //
+    //
+    let mut att_number_rw = class
+        .create_attribute("rw")
+        .with_rw()
+        .with_info(r#"read write command"#)
+        .start_as_number()
+        .await?;
+
+    //
+    //
+    tokio::spawn(async move {
+        loop {
+            att_number_rw.wait_for_commands().await;
+            while let Some(command) = att_number_rw.pop().await {
+                log_info!(att_number_rw.logger(), "command recieved - {:?}", command);
+                att_number_rw.set(command).await.unwrap();
+            }
+        }
+    });
+
+    Ok(())
+}
